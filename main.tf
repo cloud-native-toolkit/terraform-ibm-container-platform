bf1b91834363a2737ed713fd3c3be23974cea876
Sean Sundberg <seansund@us.ibm.com>
2020-05-18 18:50:38 -0500

    Removes dependency on ibmcloud cli and helm3 cli

--- main.tf
+++ main.tf
@@ -17,18 +17,6 @@ data "ibm_resource_group" "resource_group" {
   name = var.resource_group_name
 }
 
-resource "null_resource" "ibmcloud_login" {
-  provisioner "local-exec" {
-    command = "ibmcloud login -r $${REGION} -g $${RESOURCE_GROUP} --apikey $${APIKEY} > /dev/null"
-
-    environment = {
-      REGION         = var.cluster_region
-      RESOURCE_GROUP = var.resource_group_name
-      APIKEY         = var.ibmcloud_api_key
-    }
-  }
-}
-
 locals {
   cluster_config_dir    = pathexpand("~/.kube")
   cluster_type_file     = "${path.cwd}/.tmp/cluster_type.val"
@@ -108,8 +96,7 @@ data "ibm_container_vpc_cluster" "config" {
 
 data "ibm_container_cluster_config" "cluster" {
   depends_on        = [
-    ibm_container_cluster.create_cluster,
-    null_resource.ibmcloud_login,
+    ibm_container_cluster.create_cluster
   ]
 
   cluster_name_id   = local.cluster_name
@@ -117,58 +104,8 @@ data "ibm_container_cluster_config" "cluster" {
   config_dir        = local.cluster_config_dir
 }
 
-resource "null_resource" "get_cluster_version" {
-  depends_on = [
-    data.ibm_container_cluster_config.cluster,
-    null_resource.ibmcloud_login,
-  ]
-
-  provisioner "local-exec" {
-    command = "ibmcloud ks cluster get --cluster $${CLUSTER_NAME} | grep \"Version\" | sed -E \"s/Version: +(.*)$/\\1/g ; s/^([0-9]+\\.[0-9]+).*/\\1/g\" | xargs echo -n > $${FILE}"
-
-    environment = {
-      CLUSTER_NAME = local.cluster_name
-      FILE         = local.cluster_version_file
-    }
-  }
-}
-
-data "local_file" "cluster_version" {
-  depends_on = [null_resource.get_cluster_version]
-
-  filename = local.cluster_version_file
-}
-
-resource "null_resource" "oc_login" {
-  count      = var.cluster_type != "kubernetes" ? 1: 0
-
-  provisioner "local-exec" {
-    command = "oc login -u ${var.login_user} -p ${var.ibmcloud_api_key} --server=${local.server_url} > /dev/null"
-  }
-}
-
-# this should probably be moved to a separate module that operates at a namespace level
-resource "null_resource" "create_registry_namespace" {
-  depends_on = [null_resource.create_dirs]
-
-  provisioner "local-exec" {
-    command = "${path.module}/scripts/create-registry-namespace.sh ${var.resource_group_name} ${var.cluster_region} ${local.registry_url_file}"
-
-    environment = {
-      APIKEY = var.ibmcloud_api_key
-    }
-  }
-}
-
-data "local_file" "registry_url" {
-  depends_on = [null_resource.create_registry_namespace]
-
-  filename = local.registry_url_file
-}
-
 resource "null_resource" "setup_kube_config" {
   depends_on = [null_resource.create_dirs]
-  count = var.cluster_type == "kubernetes" ? 1 : 0
 
   provisioner "local-exec" {
     command = "rm -f ${local.cluster_config_dir}/config && ln -s ${data.ibm_container_cluster_config.cluster.config_file_path} ${local.cluster_config_dir}/config"
@@ -179,17 +116,8 @@ resource "null_resource" "setup_kube_config" {
   }
 }
 
-resource "null_resource" "create_cluster_pull_secret_iks" {
-  depends_on = [null_resource.setup_kube_config]
-  count      = var.cluster_type == "kubernetes" ? 1 : 0
-
-  provisioner "local-exec" {
-    command = "${path.module}/scripts/cluster-pull-secret-apply.sh ${local.cluster_name}"
-  }
-}
-
 resource "null_resource" "delete_ibmcloud_chart" {
-  depends_on = [null_resource.oc_login, null_resource.setup_kube_config]
+  depends_on = [null_resource.setup_kube_config]
 
   provisioner "local-exec" {
     command = "${path.module}/scripts/helm3-uninstall.sh ${local.ibmcloud_release_name} ${local.config_namespace}"
