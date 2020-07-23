31a166546fbc0fdc8c633e9391a8cd7efb09829b
Sean Sundberg <seansund@us.ibm.com>
2020-07-23 00:17:12 -0500

    Reads the versions from the environment instead of hard coding them

--- main-0-setup.tf
+++ main-0-setup.tf
@@ -40,12 +40,16 @@ locals {
   server_url            = var.is_vpc ? length(data.ibm_container_vpc_cluster.config) > 0 ? data.ibm_container_vpc_cluster.config[0].public_service_endpoint_url : "" : length(data.ibm_container_cluster.config) > 0 ? data.ibm_container_cluster.config[0].public_service_endpoint_url : ""
   ingress_hostname      = var.is_vpc ? length(data.ibm_container_vpc_cluster.config) > 0 ? data.ibm_container_vpc_cluster.config[0].ingress_hostname : "" : length(data.ibm_container_cluster.config) > 0 ? data.ibm_container_cluster.config[0].ingress_hostname : ""
   tls_secret            = var.is_vpc ? length(data.ibm_container_vpc_cluster.config) > 0 ? data.ibm_container_vpc_cluster.config[0].ingress_secret : "" : length(data.ibm_container_cluster.config) > 0 ? data.ibm_container_cluster.config[0].ingress_secret : ""
+  openshift_versions    = {
+  for version in data.ibm_container_cluster_versions.cluster_versions.valid_openshift_versions:
+  substr(version, 0, 3) => "${version}_openshift"
+  }
   cluster_type_cleaned  = regex("(kubernetes|iks|openshift|ocp3|ocp44|ocp4).*", var.cluster_type)[0]
   cluster_type          = local.cluster_type_cleaned == "ocp3" ? "openshift" : (local.cluster_type_cleaned == "ocp44" ? "openshift" : (local.cluster_type_cleaned == "ocp4" ? "openshift" : (local.cluster_type_cleaned == "iks" ? "kubernetes" : local.cluster_type_cleaned)))
   # value should be ocp4, ocp3, or kubernetes
   cluster_type_code     = local.cluster_type_cleaned == "openshift" ? "ocp3" : (local.cluster_type_cleaned == "ocp44" ? "ocp4" : (local.cluster_type_cleaned == "iks" ? "kubernetes" : local.cluster_type_cleaned))
   cluster_type_tag      = local.cluster_type == "kubernetes" ? "iks" : "ocp"
-  cluster_version       = local.kubernetes_versions[local.cluster_type_cleaned]
+  cluster_version       = local.cluster_type_code == "ocp4" ? local.openshift_versions["4.3"] : (local.cluster_type_code == "ocp3" ? local.openshift_versions["3.1"] : "")
   ibmcloud_release_name = "ibmcloud-config"
   registry_namespace    = var.registry_namespace != "" ? var.registry_namespace : var.resource_group_name
   vpc_zone_names        = split(",", var.vpc_zone_names)
@@ -59,4 +63,10 @@ resource "null_resource" "create_dirs" {
   provisioner "local-exec" {
     command = "mkdir -p ${local.cluster_config_dir}"
   }
+}
+
+data "ibm_container_cluster_versions" "cluster_versions" {
+  depends_on = [null_resource.create_dirs]
+
+  resource_group_id = data.ibm_resource_group.resource_group.id
 }
