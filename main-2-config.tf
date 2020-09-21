9a7d7c2009cd6b3f76f999edbaa729bf813ed17a
Sean Sundberg <seansund@us.ibm.com>
2020-09-21 17:41:03 -0500

    Adds condition on consolelink delete (#37)
    * Splits consolelink delete logic into separate conditional resource
    * Adds iks118_vpc test platform

--- main-2-config.tf
+++ main-2-config.tf
@@ -194,6 +194,12 @@ resource "null_resource" "delete-helm-cloud-config" {
       KUBECONFIG = local.cluster_config
     }
   }
+}
+
+
+resource "null_resource" "delete-consolelink" {
+  depends_on = [null_resource.setup_kube_config]
+  count      = local.cluster_type_code == "ocp4" ? 1 : 0
 
   provisioner "local-exec" {
     command = "kubectl delete consolelink toolkit-github --ignore-not-found"
@@ -235,7 +241,7 @@ resource "null_resource" "print-values" {
 }
 
 resource "helm_release" "cloud_setup" {
-  depends_on = [null_resource.setup_kube_config, null_resource.delete-helm-cloud-config, local_file.cloud-values]
+  depends_on = [null_resource.setup_kube_config, null_resource.delete-helm-cloud-config, null_resource.delete-consolelink, local_file.cloud-values]
 
   name              = "cloud-setup"
   chart             = local.chart_dir
