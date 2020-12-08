c796da0fd58141117466e097655b05d9cba7127f
Sean Sundberg <seansund@us.ibm.com>
2020-12-08 14:28:06 -0600

    Fixes naming collision

--- main-0-setup.tf
+++ main-0-setup.tf
@@ -15,7 +15,7 @@ resource "null_resource" "ibmcloud_login" {
 }
 
 locals {
-  cluster_config = {
+  config_values = {
     kubernetes = {
       type      = "kubernetes"
       type_code = "kubernetes"
@@ -68,11 +68,11 @@ locals {
   substr(version, 0, 3) => "${version}_openshift"
   }
   cluster_type_cleaned  = regex("(kubernetes|iks|openshift|ocp3|ocp44|ocp45|ocp4).*", var.cluster_type)[0]
-  cluster_type          = local.cluster_config[local.cluster_type_cleaned].type
+  cluster_type          = local.config_values[local.cluster_type_cleaned].type
   # value should be ocp4, ocp3, or kubernetes
-  cluster_type_code     = local.cluster_config[local.cluster_type_cleaned].type_code
+  cluster_type_code     = local.config_values[local.cluster_type_cleaned].type_code
   cluster_type_tag      = local.cluster_type == "kubernetes" ? "iks" : "ocp"
-  cluster_version       = local.cluster_type == "openshift" ? local.openshift_versions[local.cluster_config[local.cluster_type_cleaned].version] : ""
+  cluster_version       = local.cluster_type == "openshift" ? local.openshift_versions[local.config_values[local.cluster_type_cleaned].version] : ""
   ibmcloud_release_name = "ibmcloud-config"
   vpc_zone_names        = var.vpc_zone_names
 }
