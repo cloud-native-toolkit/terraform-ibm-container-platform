82639ab499cc1a2cb21da78d1bc20308d04eaf23
Andrew Trice <amtrice@us.ibm.com>
2021-05-21 09:16:18 -0400

    removed confing added to test stage0
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- test/stages/stage0.tf
+++ test/stages/stage0.tf
@@ -1,16 +1,2 @@
 terraform {
-  required_version = ">= 0.13.0"
-
-  required_providers {
-    ibm = {
-      source = "ibm-cloud/ibm"
-      version = ">= 1.22.0"
-    }
-  }
-}
-
-provider "ibm" {
-  region           = var.region
-  generation       = 2
-  ibmcloud_api_key = var.ibmcloud_api_key
 }
